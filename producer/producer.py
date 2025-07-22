f5f9834c56ab4bcebd96682911655646d5fb2185
AnastasiaShvydkaiia <9002051110@mail.ru>
2025-07-23 04:19:34 +0500

    Fix sensor simulation

--- producer/producer.py
+++ producer/producer.py
@@ -1,6 +1,7 @@
 import time
 import random
 import json
+import numpy as np
 from kafka import KafkaProducer
 from datetime import datetime
 import os
@@ -9,11 +10,12 @@ BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092")
 TOPIC='sensor-data'
 
 class Station:
+    """Simulates sensor data"""
     def __init__(self,station_id):
         self.station_id=station_id
-        self.base_temp=random.uniform(40,55)
-        self.base_humidity=random.uniform(35,60)
-        self.base_sound=random.uniform(50,60)
+        self.base_temp =np.random.beta(53.822, 46.706)*35+10 #10-45 degrees celcius
+        self.base_humidity = np.random.beta(1.987, 7.945)*88 # 0-88%
+        self.base_sound =np.random.beta(264.759, 261.590)*130-11 #-19-119 dB
 
     def simulate(self):
         # Simulate gradual drift around baselines using Gaussian Noise
@@ -21,10 +23,10 @@ class Station:
         humidity=self.base_humidity+random.gauss(0,5)
         sound= self.base_sound+random.gauss(0,5)
         # Simulate occasional anomalies
-        if random.random()<0.05:
-            temp+=random.uniform(10,25)
-            humidity+=random.uniform(5,10)
-            sound+=random.uniform(10,20)
+        if random.random()<0.1: # 10 % chance of anomaly
+            temp+=random.uniform(10,40)
+            humidity+=random.uniform(10,40)
+            sound+=random.uniform(10,40)
         return {
             "station_id":self.station_id,
             "timestamp":datetime.utcnow().isoformat() + 'Z',
@@ -34,9 +36,7 @@ class Station:
         }
 
 def stream_data(interval_seconds=15):
-    """
-    Continuously generates and sends data to the Kafka consumer.
-    """
+    """Continuously generates and sends data to the Kafka consumer"""
     stations=[Station(f"station_{i}") for i in range(1,3)]
     
     producer= KafkaProducer(
