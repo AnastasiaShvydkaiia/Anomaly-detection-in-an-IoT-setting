348016bf76b66c5644ce603d2c4ee4b4658c24d8
Anastasia Shvydkaiia <166436183+AnastasiaShvydkaiia@users.noreply.github.com>
2025-08-03 14:54:54 +0500

    Delete model_service directory

--- model_service/metrics.py
+++
@@ -1,7 +0,0 @@
-from prometheus_client import Counter, Gauge
-
-anomaly_counter = Counter("anomalies_total", "Total anomalies", ["station_id"])
-score_gauge = Gauge("anomaly_score", "Anomaly score", ["station_id"])
-# Initialize metrics
-for station in ["station_1", "station_2"]:
-    anomaly_counter.labels(station_id=station).inc(0)
